15558b4fda2fec6c52b79b80b2488eb228afe488
Tracy Lee <tracy@wingman.events>
2015-11-23 17:03:25 -0800

    tomster-beta

--- addon/components/spinning-tomster.js
+++ addon/components/spinning-tomster.js
@@ -2,5 +2,14 @@ import Ember from 'ember';
 import layout from '../templates/components/spinning-tomster';
 
 export default Ember.Component.extend({
-  layout: layout
+  layout: layout,
+  scrollamount: 20,
+  tomster: 'tomster',
+  tomsterImage: Ember.computed('tomster',function(){
+  	return `/spinning-tomster/images/${this.get('tomster')}.png`.htmlSafe();
+  }),
+  didInsertElement(){
+  	let marquee=this.$('marquee');
+  	marquee.on('bounce');
+  }
 });
